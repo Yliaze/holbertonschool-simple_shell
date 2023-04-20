eb0433edd949e9af045551afb501162f572f96c4
Axel Malka <axelmalka@outlook.fr>
2023-04-20 15:24:21 +0200

    fix second round

--- main.c
+++ main.c
@@ -7,17 +7,19 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, 
 	size_t len = 0;
 	pid_t child_pid = 0;
 	struct stat st;
-
+	env = clear_path(get_path(envp));
 	while (getline(&line, &len, stdin) > 0)
 	{
+		printf("env[2] = %s\n",env[2]);
 		clear_line(line);
 		a = nb_token(line, delims);
 		if (a)
 		{
 			av = cut_string(line, delims, a);
-			env = clear_path(get_path(envp));
 			av[0] = check_for_func(av[0], env);
-			printf("env[2] = %s",env[2]);
+			
+			printf("av[0] = %s\n",av[0]);
+			printf("env[2] = %s\n",env[2]);
 
 			_exec(av, envp);
 
