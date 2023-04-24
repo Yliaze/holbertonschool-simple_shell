6b64aa1a41f0de9b83bce106f65379db245438bf
Benjamin <benjamin.alazet@gmail.com>
2023-04-24 12:28:12 +0200

    Remove useless variables in main

--- main.c
+++ main.c
@@ -3,10 +3,8 @@
 int main(void)
 {
 	char delims[] = " ", *line = NULL, *env, *av[1024] = {NULL};
-	int a = 0;
+	int a = 0, i;
 	size_t len = 0;
-	pid_t child_pid = 0;
-	struct stat st;
 	char *quelquechose;
 
 	env = _gentenv("PATH");
@@ -18,7 +16,7 @@ int main(void)
 		a = nb_token(line, delims);
 		av[0] = strtok(line, delims);
 
-		for (int i = 1; i < a; i++)
+		for (i = 1; i < a; i++)
 			av[i] = strtok(NULL, delims);
 		av[0] = _which(quelquechose, av[0]);
 		if (av[0] == NULL)
